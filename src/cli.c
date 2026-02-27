77f98fe818b02fe9189edc6c381dda4d6c949736
Jared Pon <27853460+jaredponn@users.noreply.github.com>
2026-02-27 00:36:42 +0000

    WIP Likely broken draft

--- src/cli.c
+++ src/cli.c
@@ -13,12 +13,12 @@ static int parse_int(const char *str, const char *name, int *out) {
   long val = strtol(str, &end, 10);
 
   if (end == str || *end != '\0') {
-    fprintf(stderr, "Error: -%s value '%s' is not a valid integer\n", name,
+    fprintf(stderr, "Error: --%s value '%s' is not a valid integer\n", name,
             str);
     return 1;
   }
   if (errno == ERANGE || val < INT_MIN || val > INT_MAX) {
-    fprintf(stderr, "Error: -%s value '%s' is out of range\n", name, str);
+    fprintf(stderr, "Error: --%s value '%s' is out of range\n", name, str);
     return 1;
   }
 
@@ -28,14 +28,14 @@ static int parse_int(const char *str, const char *name, int *out) {
 
 void print_usage(const char *program) {
   fprintf(stderr,
-          "Usage: %s -w <width> -h <height> [-x <hdpi>] [-y <vdpi>] "
-          "<input.ttf>\n"
-          "  -w <width>   Glyph width in 1/64 of a point (a point is 1/72 of "
-          "an inch)\n"
-          "  -h <height>  Glyph height in 1/64 of point (a point is 1/72 of an "
-          "inch)\n"
-          "  -x <hdpi>    Horizontal device resolution (default: 72)\n"
-          "  -y <vdpi>    Vertical device resolution (default: 72)\n",
+          "Usage: %s --width <width> --height <height> [--hdpi <hdpi>] "
+          "[--vdpi <vdpi>] <input.ttf>\n"
+          "  --width <width>    Glyph width in 1/64 of a point (a point is "
+          "1/72 of an inch)\n"
+          "  --height <height>  Glyph height in 1/64 of point (a point is "
+          "1/72 of an inch)\n"
+          "  --hdpi <hdpi>      Horizontal device resolution (default: 72)\n"
+          "  --vdpi <vdpi>      Vertical device resolution (default: 72)\n",
           program);
 }
 
@@ -46,23 +46,31 @@ int parse_args(int argc, char *argv[], ttf_to_bw_args_t *out) {
   out->vdpi = 72;
   out->input_ttf = NULL;
 
+  static struct option long_options[] = {
+      {"width", required_argument, NULL, 'w'},
+      {"height", required_argument, NULL, 'h'},
+      {"hdpi", required_argument, NULL, 'x'},
+      {"vdpi", required_argument, NULL, 'y'},
+      {NULL, 0, NULL, 0},
+  };
+
   int opt;
-  while ((opt = getopt(argc, argv, "w:h:x:y:")) != -1) {
+  while ((opt = getopt_long(argc, argv, "", long_options, NULL)) != -1) {
     switch (opt) {
     case 'w':
-      if (parse_int(optarg, "w", &out->width))
+      if (parse_int(optarg, "width", &out->width))
         return 1;
       break;
     case 'h':
-      if (parse_int(optarg, "h", &out->height))
+      if (parse_int(optarg, "height", &out->height))
         return 1;
       break;
     case 'x':
-      if (parse_int(optarg, "x", &out->hdpi))
+      if (parse_int(optarg, "hdpi", &out->hdpi))
         return 1;
       break;
     case 'y':
-      if (parse_int(optarg, "y", &out->vdpi))
+      if (parse_int(optarg, "vdpi", &out->vdpi))
         return 1;
       break;
     default:
@@ -76,8 +84,8 @@ int parse_args(int argc, char *argv[], ttf_to_bw_args_t *out) {
   }
   out->input_ttf = argv[optind];
 
-  if (out->width <= 0 || out->height <= 0) {
-    fprintf(stderr, "Error: width and height must be positive integers\n");
+  if (!(out->width >= 0 && out->height >= 0)) {
+    fprintf(stderr, "Error: width and height must be non-negative integers\n");
     return 1;
   }
 
--- src/main.c
+++ src/main.c
@@ -4,8 +4,11 @@
  */
 #include "cli.h"
 
+#include <errno.h>
+#include <inttypes.h>
 #include <stdint.h>
 #include <stdio.h>
+#include <string.h>
 
 #include <ft2build.h>
 #include FT_FREETYPE_H
@@ -74,29 +77,136 @@ int main(int argc, char *argv[]) {
   }
 
   /////////////////////////
-  // Creating the header
+  // Open output files
   /////////////////////////
-
-  /////////////////////////
-  // Creating the mapping
-  /////////////////////////
-  printf("#ifndef TTF_TO_BW_H\n");
-  printf("#define TTF_TO_BW_H\n");
-  for (uint8_t i = 0; i < UINT8_MAX; ++i) {
-    ft_error = FT_Load_Char(face, i, FT_LOAD_RENDER | FT_LOAD_MONOCHROME);
-
-    if (ft_error != FT_Err_Ok) {
-      fprintf(stderr, "loading char failed for %X with ft_error=%s\n", i,
-              FT_Error_String(ft_error) == NULL ? "<unknown error>"
-                                                : FT_Error_String(ft_error));
-    }
+  FILE *header_file = fopen("ttf_to_bw.h", "w");
+  if (header_file == NULL) {
+    fprintf(stderr, "Failed to open ttf_to_bw.h for writing: %s\n",
+            strerror(errno));
+    status = 1;
+    goto ft_done_face;
   }
 
-  printf("#endif TTF_TO_BW_H\n");
+  FILE *source_file = fopen("ttf_to_bw.c", "w");
+  if (source_file == NULL) {
+    fprintf(stderr, "Failed to open ttf_to_bw.c for writing: %s\n",
+            strerror(errno));
+    status = 1;
+    goto close_header;
+  }
+
+  /////////////////////////
+  // Creating the header
+  /////////////////////////
+  fprintf(header_file, "#ifndef TTF_TO_BW_H\n");
+  fprintf(header_file, "#define TTF_TO_BW_H\n");
+
+  fprintf(header_file, "#include <stdint.h>\n");
+
+  fprintf(header_file,
+          "/** See <https://freetype.org/freetype2/docs/tutorial/example1.c> "
+          "as an example of\n");
+  fprintf(header_file, " * the underlying freetype library with how to use\n");
+  fprintf(header_file, " */\n");
+  fprintf(header_file, "typedef struct {\n");
+  fprintf(header_file, "  int bitmap_left; //\n");
+  fprintf(header_file, "  int bitmap_top; //\n");
+  fprintf(header_file, "  long int advance_x; //\n");
+  fprintf(header_file, "  long int advance_y; //\n");
+  fprintf(header_file, "  unsigned int rows; // number of bitmap rows\n");
+  fprintf(header_file,
+          "  unsigned int width; // number of pixels in bitmap row\n");
+  fprintf(header_file, "  int pitch; // pitch is an offset to add to a bitmap "
+                       "pointer in order to go down one row.\n");
+  fprintf(header_file, "  unsigned char const *buffer;\n");
+  fprintf(header_file, "} ttf_to_bw_slot_t;\n");
+  fprintf(header_file, "\n");
+
+  fprintf(header_file, "\n");
+
+  fprintf(header_file, "/** The character mapping from ASCII characters to the "
+                       "useful information to render the character");
+  fprintf(header_file, " * the underlying freetype library with how to use\n");
+  fprintf(header_file, " */\n");
+  fprintf(header_file,
+          "extern ttf_to_bw_slot_t const ttf_to_bw_character_map[UINT8_MAX];");
+
+  fprintf(header_file, "#endif /* TTF_TO_BW_H */\n");
+
+  /////////////////////////
+  // Creating the source
+  /////////////////////////
+  fprintf(source_file, "#include \"ttf_to_bw.h\"\n");
+  fprintf(source_file, "\n");
+
+  // First, create buffers of all the bit packed data
+  for (uint8_t character = 0; character < UINT8_MAX; ++character) {
+    ft_error =
+        FT_Load_Char(ft_face, character, FT_LOAD_RENDER | FT_LOAD_MONOCHROME);
+
+    if (ft_error != FT_Err_Ok) {
+      fprintf(stderr, "loading char failed for %X with ft_error=%s\n",
+              character,
+              FT_Error_String(ft_error) == NULL ? "<unknown error>"
+                                                : FT_Error_String(ft_error));
+      continue;
+    }
+
+    FT_GlyphSlot ft_slot = ft_face->glyph;
+
+    fprintf(source_file,
+            "static unsigned char const char_buffer_%02" PRIu8 "[] = {",
+            (int)character);
+    size_t bitmap_index = 0;
+    for (size_t bitmap_column = 0; bitmap_column < ft_slot->bitmap.width;
+         ++bitmap_column) {
+      for (size_t bitmap_row = 0; bitmap_row < ft_slot->bitmap.rows;
+           ++bitmap_row) {
+        fprintf(source_file, "0x%02X,", ft_slot->bitmap.buffer[bitmap_index++]);
+      }
+    }
+    fprintf(source_file, "};\n");
+  }
+
+  // Then, we create the mapping
+  fprintf(source_file,
+          "ttf_to_bw_slot_t const ttf_to_bw_character_map[UINT8_MAX] = {\n");
+
+  for (uint8_t character = 0; character < UINT8_MAX; ++character) {
+    ft_error =
+        FT_Load_Char(ft_face, character, FT_LOAD_RENDER | FT_LOAD_MONOCHROME);
+
+    if (ft_error != FT_Err_Ok) {
+      fprintf(stderr, "loading char failed for %X with ft_error=%s\n",
+              character,
+              FT_Error_String(ft_error) == NULL ? "<unknown error>"
+                                                : FT_Error_String(ft_error));
+
+      fprintf(source_file, " { 0 },\n");
+      continue;
+    }
+
+    FT_GlyphSlot ft_slot = ft_face->glyph;
+
+    fprintf(source_file,
+            " { .bitmap_left = %d, .bitmap_top = %d, .advance_x = %ld, "
+            ".advance_y = %ld, .rows = %d, .width = %d, .pitch = %d, .buffer = "
+            "char_buffer_%02" PRIu8 " },\n",
+            ft_slot->bitmap_left, ft_slot->bitmap_top, ft_slot->advance.x,
+            ft_slot->advance.y, ft_slot->bitmap.rows, ft_slot->bitmap.width,
+            ft_slot->bitmap.pitch, character);
+  }
+  fprintf(source_file, "};\n");
 
   /////////////////////////
   // Cleanup
   /////////////////////////
+close_source:
+  fclose(source_file);
+
+close_header:
+  fclose(header_file);
+
 ft_done_face:
   FT_Done_Face(ft_face);
 
