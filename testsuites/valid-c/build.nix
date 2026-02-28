60abf850e9948c774ba0cc1b183e1213ce5ce2c6
Jared Pon <27853460+jaredponn@users.noreply.github.com>
2026-02-28 01:18:33 +0000

    Move fixtures --> font-fixtures

--- testsuites/valid-c/build.nix
+++ testsuites/valid-c/build.nix
@@ -9,7 +9,7 @@
 
       buildPhase = ''
         set -eu
-        export TTF=${../fixtures/JetBrains_Mono/static/JetBrainsMono-Regular.ttf}
+        export TTF=${../font-fixtures/JetBrains_Mono/static/JetBrainsMono-Regular.ttf}
         export TEST_CASES_DIRECTORY=
 
         bash ${./run-tests.sh} ${./test-cases}
